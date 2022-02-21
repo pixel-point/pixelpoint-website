a75199ecdc5403ac0909807fc1788841d0c6224f
Maksim Gorodov <silencer.web@gmail.com>
2022-02-21 15:32:29 +0500

    feat: remove pagination from blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const { BLOG_BASE_PATH, BLOG_POSTS_PER_PAGE } = require('./src/constants/blog');
+const { BLOG_BASE_PATH } = require('./src/constants/blog');
 const getBlogPostPath = require('./src/utils/get-blog-post-path');
 
 // We have this variable in order to decide whether to render draft posts or not
@@ -9,44 +9,16 @@ const getBlogPostPath = require('./src/utils/get-blog-post-path');
 // We have an array structure here in order to use it in the filter using the "in" operator
 const DRAFT_FILTER = process.env.NODE_ENV === 'production' ? [false] : [true, false];
 
-async function createBlogPages({ graphql, actions }) {
+async function createBlogPage({ actions }) {
   const { createPage } = actions;
 
-  const result = await graphql(
-    `
-      query ($draftFilter: [Boolean]!) {
-        allMdx(
-          filter: {
-            fileAbsolutePath: { regex: "/posts/" }
-            fields: { draft: { in: $draftFilter } }
-          }
-        ) {
-          totalCount
-        }
-      }
-    `,
-    { draftFilter: DRAFT_FILTER }
-  );
-
-  if (result.errors) throw new Error(result.errors);
-
-  const pageCount = Math.ceil(result.data.allMdx.totalCount / BLOG_POSTS_PER_PAGE);
-
-  Array.from({ length: pageCount }).forEach((_, i) => {
-    const pagePath = i === 0 ? BLOG_BASE_PATH : `${BLOG_BASE_PATH}/${i + 1}`;
-
-    createPage({
-      path: pagePath,
-      component: path.resolve('./src/templates/blog.jsx'),
-      context: {
-        currentPageIndex: i,
-        pageCount,
-        limit: BLOG_POSTS_PER_PAGE,
-        skip: i * BLOG_POSTS_PER_PAGE,
-        draftFilter: DRAFT_FILTER,
-        canonicalUrl: `${process.env.GATSBY_DEFAULT_SITE_URL}${pagePath}`,
-      },
-    });
+  createPage({
+    path: BLOG_BASE_PATH,
+    component: path.resolve('./src/templates/blog.jsx'),
+    context: {
+      draftFilter: DRAFT_FILTER,
+      canonicalUrl: `${process.env.GATSBY_DEFAULT_SITE_URL}${BLOG_BASE_PATH}`,
+    },
   });
 }
 
@@ -104,6 +76,6 @@ exports.onCreateNode = ({ node, actions }) => {
 };
 
 exports.createPages = async (options) => {
-  await createBlogPages(options);
+  await createBlogPage(options);
   await createBlogPosts(options);
 };
--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -3,7 +3,6 @@ import { graphql } from 'gatsby';
 import React from 'react';
 
 import Hero from 'components/pages/blog/hero';
-import Pagination from 'components/pages/blog/pagination';
 import PostsList from 'components/pages/blog/posts-list';
 import CaseStudies from 'components/shared/case-studies';
 import CTA from 'components/shared/cta';
@@ -13,24 +12,20 @@ const BlogTemplate = ({
   data: {
     allMdx: { nodes },
   },
-  pageContext: { currentPageIndex, pageCount },
 }) => (
   <Layout>
     <Hero />
     <PostsList items={nodes} />
-    {pageCount > 1 && <Pagination currentPageIndex={currentPageIndex} pageCount={pageCount} />}
     <CaseStudies />
     <CTA />
   </Layout>
 );
 
 export const query = graphql`
-  query ($limit: Int!, $skip: Int!, $draftFilter: [Boolean]!) {
+  query ($draftFilter: [Boolean]!) {
     allMdx(
       filter: { fileAbsolutePath: { regex: "/posts/" }, fields: { draft: { in: $draftFilter } } }
       sort: { order: DESC, fields: slug }
-      limit: $limit
-      skip: $skip
     ) {
       nodes {
         slug
