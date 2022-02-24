6afc3fa555a54fbf0d427049a0799813806f1243
Maksim Gorodov <silencer.web@gmail.com>
2022-02-24 13:13:44 +0500

    fix: add some fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'caseStudies',
+        name: 'case-studies',
         path: `${__dirname}/src/case-studies`,
       },
     },
