8157deeebbc19715d459b004deeed45a90ca96a9
Maksim Gorodov <silencer.web@gmail.com>
2022-02-24 17:32:12 +0500

    feat: update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,12 @@ require('dotenv').config();
 module.exports = {
   flags: { DEV_SSR: process.env.GATSBY_DEV_SSR || false },
   siteMetadata: {
-    siteTitle: 'Pixel Point Gatsby Tailwind Starter',
-    siteDescription: 'Site Description',
+    siteTitle: 'Pixel Point – Design and Development',
+    siteDescription:
+      'Get web design, development with Pixel Point for a fair price and great quality.',
     siteImage: '/images/social-preview.jpg',
     siteLanguage: 'en',
     siteUrl: process.env.GATSBY_DEFAULT_SITE_URL || 'http://localhost:8000',
-    authorName: 'Pixel Point',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
