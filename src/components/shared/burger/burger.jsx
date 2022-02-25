bee81d0348fba600cf59822d12623b16e7ff10ff
Maksim Gorodov <silencer.web@gmail.com>
2022-02-25 14:33:28 +0500

    feat: add mobile menu

--- src/components/shared/burger/burger.jsx
+++ src/components/shared/burger/burger.jsx
@@ -7,31 +7,28 @@ const ANIMATION_DURATION = 0.2;
 
 const Burger = ({ className: additionalClassName, isToggled, onClick }) => (
   <motion.button
-    // TODO: Add border color, e.g. "border-white"
-    className={clsx('relative h-10 w-10 rounded-full border-2', additionalClassName)}
+    className={clsx('relative h-5 w-7 text-white', isToggled && 'text-black', additionalClassName)}
     type="button"
     animate={isToggled ? 'toggled' : 'initial'}
     onClick={onClick}
   >
     <motion.span
-      // TODO: Add background color, e.g. "bg-white"
-      className="absolute top-[11px] left-[8px] block h-0.5 w-5 rounded-full"
+      className="absolute top-0 right-0 block h-0.5 w-full rounded-full bg-current transition-colors duration-200"
       variants={{
         initial: {
-          top: 11,
+          top: 0,
           display: 'block',
           transition: { duration: ANIMATION_DURATION, delay: ANIMATION_DURATION },
         },
         toggled: {
-          top: 17,
+          top: 9,
           transition: { duration: ANIMATION_DURATION },
           transitionEnd: { display: 'none' },
         },
       }}
     />
     <motion.span
-      // TODO: Add background color, e.g. "bg-white"
-      className="absolute top-[17px] left-[12px] block h-0.5 w-3 rounded-full"
+      className="absolute top-[9px] right-0 block h-0.5 w-5 rounded-full bg-current transition-colors duration-200"
       variants={{
         initial: {
           display: 'block',
@@ -44,24 +41,22 @@ const Burger = ({ className: additionalClassName, isToggled, onClick }) => (
       }}
     />
     <motion.span
-      // TODO: Add background color, e.g. "bg-white"
-      className="absolute bottom-[11px] left-[8px] block h-0.5 w-5 rounded-full"
+      className="absolute bottom-0 right-0 block h-0.5 w-full rounded-full bg-current transition-colors duration-200"
       variants={{
         initial: {
-          bottom: 11,
+          bottom: 0,
           display: 'block',
           transition: { duration: ANIMATION_DURATION, delay: ANIMATION_DURATION },
         },
         toggled: {
-          bottom: 17,
+          bottom: 9,
           transition: { duration: ANIMATION_DURATION },
           transitionEnd: { display: 'none' },
         },
       }}
     />
     <motion.span
-      // TODO: Add background color, e.g. "bg-white"
-      className="absolute top-[17px] left-[8px] hidden h-0.5 w-5 rounded-full"
+      className="absolute top-[9px] right-0 hidden h-0.5 w-full rounded-full bg-current transition-colors duration-200"
       variants={{
         initial: {
           rotate: '0deg',
@@ -76,8 +71,7 @@ const Burger = ({ className: additionalClassName, isToggled, onClick }) => (
       }}
     />
     <motion.span
-      // TODO: Add background color, e.g. "bg-white"
-      className="absolute top-[17px] left-[8px] hidden h-0.5 w-5 rounded-full"
+      className="absolute top-[9px] right-0 hidden h-0.5 w-full rounded-full bg-current transition-colors duration-200"
       variants={{
         initial: {
           rotate: '0deg',
--- src/components/shared/header/header.jsx
+++ src/components/shared/header/header.jsx
@@ -1,7 +1,8 @@
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { forwardRef } from 'react';
 
+import Burger from 'components/shared/burger';
 import Link from 'components/shared/link';
 import Logo from 'images/logo.inline.svg';
 
@@ -11,7 +12,6 @@ import ServicesWebDevelopmentIcon from './images/services-web-development.inline
 const links = [
   {
     text: 'Services',
-    to: '#',
     items: [
       {
         icon: ServicesWebDesignIcon,
@@ -41,22 +41,26 @@ const links = [
   },
 ];
 
-// TODO: Implement mobile menu functionality and delete eslint comment below, example — https://user-images.githubusercontent.com/20713191/144221747-70dc933e-a5bd-4586-9019-08117afc13e0.png
-// eslint-disable-next-line no-unused-vars
-const Header = ({ isMobileMenuOpen, onBurgerClick }) => (
-  <header className="safe-paddings absolute top-0 left-0 right-0 z-10 w-full">
+const Header = forwardRef(({ isMobileMenuOpen, onBurgerClick }, ref) => (
+  <header className="safe-paddings absolute top-0 left-0 right-0 z-50 w-full" ref={ref}>
     <div className="container-lg flex h-[88px] items-center justify-between lg:h-20 sm:h-16">
       <Link to="/">
         <span className="sr-only">Pixel Point</span>
-        <Logo className="h-9 sm:h-8" aria-hidden />
+        <Logo
+          className={clsx(
+            'h-9 transition-all duration-200 sm:h-8',
+            isMobileMenuOpen && 'md:invert'
+          )}
+          aria-hidden
+        />
       </Link>
-      <nav className="flex items-center md:hidden">
-        <ul className="flex items-center space-x-10 text-[0px]">
+      <nav className="flex items-center">
+        <ul className="flex items-center space-x-10 text-[0px] md:hidden">
           {links.map(({ text, to, items }, index) => (
             <li
               className={clsx(
                 items?.length > 0 &&
-                  'group relative after:absolute after:top-2 after:-right-1 after:h-0 after:w-0 after:translate-x-full after:border-[3px] after:border-b-0 after:border-t-white after:transition-colors after:duration-200 after:hover:border-t-red'
+                  'group relative cursor-pointer pr-2.5 after:absolute after:top-2 after:right-0 after:h-0 after:w-0 after:border-[3px] after:border-b-0 after:border-t-white after:transition-colors after:duration-200 after:hover:border-t-red'
               )}
               key={index}
             >
@@ -66,7 +70,12 @@ const Header = ({ isMobileMenuOpen, onBurgerClick }) => (
 
               {items?.length > 0 && (
                 <div className="invisible absolute -left-24 bottom-0 w-[500px] translate-y-full pt-6 text-[0px] opacity-0 transition-[opacity,visibility] duration-200 group-hover:visible group-hover:opacity-100">
-                  <ul className="rounded-2xl bg-white p-6 pb-7">
+                  <ul
+                    className="rounded-2xl bg-white p-6 pb-7"
+                    style={{
+                      boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.05), 0px 6px 20px rgba(0, 0, 0, 0.1)',
+                    }}
+                  >
                     {items.map(({ icon: Icon, text, description, linkText, to }, index) => (
                       <li
                         className={clsx(index !== 0 && 'mt-7 border-t border-t-gray-4 pt-7')}
@@ -96,10 +105,12 @@ const Header = ({ isMobileMenuOpen, onBurgerClick }) => (
             </Link>
           </li>
         </ul>
+
+        <Burger className="hidden md:block" isToggled={isMobileMenuOpen} onClick={onBurgerClick} />
       </nav>
     </div>
   </header>
-);
+));
 
 Header.propTypes = {
   isMobileMenuOpen: PropTypes.bool,
