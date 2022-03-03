cb78a53865a221bd84fc13f8641000435266d24d
Maksim Gorodov <silencer.web@gmail.com>
2022-03-03 19:49:19 +0400

    feat: add responsive styles for the page Home

--- src/components/shared/case-studies/case-studies.jsx
+++ src/components/shared/case-studies/case-studies.jsx
@@ -51,7 +51,7 @@ const CaseStudies = ({ title, itemsType }) => {
   return (
     <section className="safe-paddings mt-52 lg:mt-44 md:mt-36 sm:mt-20">
       <div className="container">
-        <h2 className="max-w-[800px] text-4xl font-normal leading-snug lg:max-w-[650px] lg:text-3xl md:max-w-[500px] md:text-2xl sm:text-xl">
+        <h2 className="max-w-[800px] text-4xl font-normal leading-snug lg:max-w-[650px] lg:text-3xl md:max-w-[500px] md:text-2xl sm:max-w-[420px] sm:text-xl">
           {title}
         </h2>
         <div className="grid-gap-x mt-16 grid grid-cols-3 gap-y-16 lg:mt-12 lg:gap-y-12 md:mt-10 md:grid-cols-2 md:gap-y-10 sm:mt-8 sm:block sm:space-y-8">
