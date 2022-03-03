98e644a69f1ead834133be309ee18588a1b950fa
Maksim Gorodov <silencer.web@gmail.com>
2022-03-03 15:59:38 +0400

    feat: add a link to all case studies in the shared component CaseStudies

--- src/components/shared/link/link.jsx
+++ src/components/shared/link/link.jsx
@@ -3,8 +3,8 @@ import { Link as GatsbyLink } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import Arrow6xl from './images/arrow-6xl.inline.svg';
 import ArrowBase from './images/arrow-base.inline.svg';
+import ArrowLarge from './images/arrow-large.inline.svg';
 
 const styles = {
   base: 'inline-flex leading-none transition-colors duration-200',
@@ -12,7 +12,11 @@ const styles = {
     '6xl': {
       wrapper:
         '!inline-block text-6xl font-normal leading-snug lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl',
-      icon: 'ml-4 h-7 lg:ml-3 !inline-block lg:h-6 md:ml-2 md:h-5 sm:ml-1.5 sm:h-4 xs:ml-1 xs:h-3',
+      icon: 'ml-4 h-7 lg:ml-3 !inline-block align-baseline lg:h-6 md:ml-2 md:h-5 sm:ml-1.5 sm:h-4 xs:ml-1 xs:h-3',
+    },
+    '2xl': {
+      wrapper: '!inline-block text-2xl font-semibold leading-snug lg:text-xl sm:text-lg',
+      icon: 'ml-3 h-4 lg:ml-3 align-baseline !inline-block lg:h-3.5 md:ml-2.5 sm:ml-2 sm:h-3',
     },
     base: {
       wrapper: 'text-base sm:text-sm',
@@ -48,8 +52,8 @@ const Link = ({ className: additionalClassName, size, theme, to, children, ...pr
         {size === 'base' && (
           <ArrowBase className={clsx(styles.size[size]?.icon, styles.theme[theme]?.icon)} />
         )}
-        {size === '6xl' && (
-          <Arrow6xl className={clsx(styles.size[size]?.icon, styles.theme[theme]?.icon)} />
+        {(size === '2xl' || size === '6xl') && (
+          <ArrowLarge className={clsx(styles.size[size]?.icon, styles.theme[theme]?.icon)} />
         )}
       </>
     ) : (
--- src/pages/services/web-development.jsx
+++ src/pages/services/web-development.jsx
@@ -7,6 +7,7 @@ import Lighthouse from 'components/pages/services-web-development/lighthouse';
 import Tailwindcss from 'components/pages/services-web-development/tailwindcss';
 import ThirdPartiesAndIntegrations from 'components/pages/services-web-development/third-parties-and-integrations';
 import Blog from 'components/shared/blog';
+import CaseStudies from 'components/shared/case-studies';
 import CTA from 'components/shared/cta';
 import Layout from 'components/shared/layout';
 import ServicesCTA from 'components/shared/services-cta';
@@ -25,6 +26,7 @@ const ServicesWebDevelopmentPage = () => (
     <CodeQuality />
     <ThirdPartiesAndIntegrations />
     <Lighthouse />
+    <CaseStudies itemsType="open-source" />
     <Blog />
     <ServicesCTA {...servicesCTA} />
     <CTA />
