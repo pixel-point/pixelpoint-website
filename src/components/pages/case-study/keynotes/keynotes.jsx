7d65587b6843c74734e0adea166bb2ed6d9493dd
Maksim Gorodov <silencer.web@gmail.com>
2022-03-04 18:44:38 +0400

    feat: add responsive styles for the template Case Study

--- src/components/pages/case-study/keynotes/keynotes.jsx
+++ src/components/pages/case-study/keynotes/keynotes.jsx
@@ -12,23 +12,31 @@ import illustration from './images/illustration.svg';
 const icons = [Icon1, Icon2, Icon3, Icon4, Icon5, Icon6];
 
 const Keynotes = ({ items }) => (
-  <section className="safe-paddings bg-black pt-32 text-white">
+  <section className="safe-paddings bg-black pt-32 text-white lg:pt-28 md:pt-24 sm:pt-16">
     <div className="container">
-      <h2 className="text-4xl font-normal leading-snug">Key Results and Outcomes</h2>
-      <ul className="grid-gap-x mt-16 grid grid-cols-3 gap-y-16">
+      <h2 className="text-4xl font-normal leading-snug lg:text-3xl md:text-2xl sm:text-xl">
+        Key Results and Outcomes
+      </h2>
+      <ul className="grid-gap-x mt-16 grid grid-cols-3 gap-y-16 lg:mt-12 lg:gap-y-12 md:mt-10 md:grid-cols-2 md:gap-y-10 sm:mt-8 sm:block sm:space-y-8">
         {items.map((item, index) => {
           const Icon = icons[index];
 
           return (
             <li className="max-w-[275px]" key={index}>
-              <Icon className="h-16" aria-hidden />
-              <p className="mt-2.5 font-normal leading-snug">{item}</p>
+              <Icon className="h-16 md:h-14" aria-hidden />
+              <p className="mt-3 font-normal leading-snug md:mt-2.5">{item}</p>
             </li>
           );
         })}
       </ul>
     </div>
-    <img className="mx-auto mt-32" src={illustration} loading="lazy" alt="" aria-hidden />
+    <img
+      className="mx-auto mt-32 lg:mt-28 md:mt-24 sm:mt-16"
+      src={illustration}
+      loading="lazy"
+      alt=""
+      aria-hidden
+    />
   </section>
 );
 
