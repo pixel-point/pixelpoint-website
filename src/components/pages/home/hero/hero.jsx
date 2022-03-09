91847982603b559a277f29b2c80b3ccaf632fafb
Maksim Gorodov <silencer.web@gmail.com>
2022-03-09 18:02:08 +0400

    feat: add text animation in the section Hero for the page Home

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -1,3 +1,5 @@
+import { motion, useAnimation } from 'framer-motion';
+import PropTypes from 'prop-types';
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { useRive, useStateMachineInput, Layout, Fit, Alignment } from 'rive-react';
@@ -9,10 +11,136 @@ import lgIllustration3 from './images/lg-illustration-3.svg';
 const STATE_MACHINE_NAME = 'State Machine';
 const INPUT_NAME = 'Fall Trigger';
 
-const Hero = () => {
-  const [wrapperRef, isWrapperInView] = useInView();
+const OPACITY_DURATION = 0.05;
+const COLOR_DURATION = 0.2;
+const COLOR_DELAY = 0.3;
 
-  const params = {
+const firstSectionTitleItems = [
+  { value: 'Marketing', delay: { opacity: OPACITY_DURATION * 0 } },
+  { value: 'website', delay: { opacity: OPACITY_DURATION * 1 } },
+  { value: 'is', delay: { opacity: OPACITY_DURATION * 2 } },
+  { value: 'a', delay: { opacity: OPACITY_DURATION * 3 } },
+  {
+    value: 'complex',
+    color: '#2b4bee',
+    delay: { opacity: OPACITY_DURATION * 4, color: OPACITY_DURATION * 4 + COLOR_DELAY },
+  },
+  { value: 'thing', delay: { opacity: OPACITY_DURATION * 5 + COLOR_DURATION + COLOR_DELAY } },
+  { value: 'to', delay: { opacity: OPACITY_DURATION * 6 + COLOR_DURATION + COLOR_DELAY } },
+  { value: 'build', delay: { opacity: OPACITY_DURATION * 7 + COLOR_DURATION + COLOR_DELAY } },
+  { value: 'on', delay: { opacity: OPACITY_DURATION * 8 + COLOR_DURATION + COLOR_DELAY } },
+  {
+    value: 'your own',
+    color: '#ee2b6c',
+    delay: {
+      opacity: OPACITY_DURATION * 9 + COLOR_DURATION + COLOR_DELAY,
+      color: OPACITY_DURATION * 9 + COLOR_DURATION + COLOR_DELAY + COLOR_DELAY,
+    },
+  },
+];
+
+const secondSectionTitleItems = [
+  { value: 'You', delay: { opacity: OPACITY_DURATION * 0 } },
+  { value: 'either', delay: { opacity: OPACITY_DURATION * 1 } },
+  { value: 'ask', delay: { opacity: OPACITY_DURATION * 2 } },
+  { value: 'your', delay: { opacity: OPACITY_DURATION * 3 } },
+  {
+    value: 'team',
+    color: '#2b4bee',
+    delay: { opacity: OPACITY_DURATION * 4, color: OPACITY_DURATION * 4 + COLOR_DELAY },
+  },
+  { value: 'of', delay: { opacity: OPACITY_DURATION * 5 + COLOR_DURATION + COLOR_DELAY } },
+  { value: 'specialists', delay: { opacity: OPACITY_DURATION * 6 + COLOR_DURATION + COLOR_DELAY } },
+  { value: 'from', delay: { opacity: OPACITY_DURATION * 7 + COLOR_DURATION + COLOR_DELAY } },
+  {
+    value: 'adjacent fields',
+    color: '#ee2b6c',
+    delay: {
+      opacity: OPACITY_DURATION * 8 + COLOR_DURATION + COLOR_DELAY,
+      color: OPACITY_DURATION * 8 + COLOR_DURATION + COLOR_DELAY + COLOR_DELAY,
+    },
+  },
+];
+
+const thirdSectionTitleItems = [
+  { value: 'Or', delay: { opacity: OPACITY_DURATION * 0 } },
+  { value: 'go', delay: { opacity: OPACITY_DURATION * 1 } },
+  { value: 'wild', delay: { opacity: OPACITY_DURATION * 2 } },
+  {
+    value: 'cherry-picking',
+    color: '#2b4bee',
+    delay: { opacity: OPACITY_DURATION * 3, color: OPACITY_DURATION * 3 + COLOR_DELAY },
+  },
+  { value: '&', delay: { opacity: OPACITY_DURATION * 4 + COLOR_DURATION + COLOR_DELAY } },
+  { value: 'managing', delay: { opacity: OPACITY_DURATION * 5 + COLOR_DURATION + COLOR_DELAY } },
+  {
+    value: 'freelancers',
+    color: '#ee2b6c',
+    delay: {
+      opacity: OPACITY_DURATION * 6 + COLOR_DURATION + COLOR_DELAY,
+      color: OPACITY_DURATION * 6 + COLOR_DURATION + COLOR_DELAY + COLOR_DELAY,
+    },
+  },
+];
+
+const titleSpanVariants = {
+  initial: ({ color }) => ({
+    opacity: 0,
+    ...(color && { color }),
+  }),
+  animate: ({ delay }) => ({
+    opacity: 1,
+    color: '#ffffff',
+    transition: {
+      opacity: { duration: OPACITY_DURATION, delay: delay.opacity },
+      color: { duration: COLOR_DURATION, delay: delay.color },
+    },
+  }),
+};
+
+const Title = ({ items, controls }) => (
+  <h2 className="max-w-[592px] lg:max-w-none">
+    {items.map(({ value, color, delay }, index) => (
+      <motion.span
+        initial="initial"
+        animate={controls}
+        variants={titleSpanVariants}
+        custom={{ color, delay }}
+        key={index}
+      >
+        {value}{' '}
+      </motion.span>
+    ))}
+  </h2>
+);
+
+Title.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      value: PropTypes.string.isRequired,
+      color: PropTypes.string,
+      delay: PropTypes.exact({
+        opacity: PropTypes.number.isRequired,
+        color: PropTypes.number,
+      }).isRequired,
+    })
+  ).isRequired,
+  controls: PropTypes.object.isRequired,
+};
+
+const Hero = () => {
+  const [wrapperRef, isWrapperInView] = useInView({ triggerOnce: true });
+  const [secondSectionRef, isSecondSectionInView] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+  const [thirdSectionRef, isThirdSectionInView] = useInView({ triggerOnce: true, threshold: 0.5 });
+
+  const firstSectionTitleControls = useAnimation();
+  const secondSectionTitleControls = useAnimation();
+  const thirdSectionTitleControls = useAnimation();
+
+  const { RiveComponent, rive } = useRive({
     src: '/animations/pages/home/hero.riv',
     autoplay: false,
     stateMachines: STATE_MACHINE_NAME,
@@ -20,9 +148,7 @@ const Hero = () => {
       fit: Fit.FitWidth,
       alignment: Alignment.TopCenter,
     }),
-  };
-
-  const { RiveComponent, rive } = useRive(params);
+  });
 
   const containerRef = useRef();
   const firstSectionRef = useRef();
@@ -41,9 +167,21 @@ const Hero = () => {
 
   useEffect(() => {
     if (isWrapperInView && rive && currentAnimState !== 'fall') {
-      rive.play('State Machine');
+      firstSectionTitleControls.start('animate').then(() => {
+        rive.play('State Machine');
+      });
     }
-  }, [isWrapperInView, rive, currentAnimState]);
+  }, [isWrapperInView, rive, currentAnimState, firstSectionTitleControls]);
+
+  useEffect(() => {
+    if (currentAnimState === 'fall' && isSecondSectionInView)
+      secondSectionTitleControls.start('animate');
+  }, [currentAnimState, isSecondSectionInView, secondSectionTitleControls]);
+
+  useEffect(() => {
+    if (currentAnimState === 'fall' && isThirdSectionInView)
+      thirdSectionTitleControls.start('animate');
+  }, [currentAnimState, isThirdSectionInView, thirdSectionTitleControls]);
 
   useEffect(() => {
     // Add event listener to window scroll
@@ -68,9 +206,7 @@ const Hero = () => {
       <div className="container grid-gap-x relative grid grid-cols-2 lg:block" ref={containerRef}>
         <div className="relative z-10 text-6xl font-normal leading-snug lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl">
           <div className="flex h-screen items-center lg:block lg:h-auto" ref={firstSectionRef}>
-            <h2 className="max-w-[592px] lg:max-w-none">
-              Marketing website is a complex thing to build on your own
-            </h2>
+            <Title items={firstSectionTitleItems} controls={firstSectionTitleControls} />
             <img
               className="mx-auto mt-16 hidden max-w-[620px] lg:block md:mt-12 md:max-w-[440px] sm:mt-8 sm:max-w-full"
               src={lgIllustration1}
@@ -78,10 +214,12 @@ const Hero = () => {
               aria-hidden
             />
           </div>
-          <div className="flex items-center pt-[100px] lg:block lg:pt-44 md:pt-36 sm:pt-20">
-            <h2 className="max-w-[592px] lg:max-w-none">
-              You either ask your team of specialists from adjacent fields
-            </h2>
+          <div
+            className="flex items-center pt-[100px] lg:block lg:pt-44 md:pt-36 sm:pt-20"
+            ref={secondSectionRef}
+          >
+            <Title items={secondSectionTitleItems} controls={secondSectionTitleControls} />
+
             <img
               className="mx-auto mt-16 hidden max-w-[620px] lg:block md:mt-12 md:max-w-[440px] sm:mt-8 sm:max-w-full"
               src={lgIllustration2}
@@ -89,10 +227,11 @@ const Hero = () => {
               aria-hidden
             />
           </div>
-          <div className="flex items-center pt-[700px] pb-[360px] lg:block lg:py-44 md:py-36 sm:py-20">
-            <h2 className="max-w-[592px] lg:max-w-none">
-              Or go wild cherry-picking & managing freelancers
-            </h2>
+          <div
+            className="flex items-center pt-[700px] pb-[360px] lg:block lg:py-44 md:py-36 sm:py-20"
+            ref={thirdSectionRef}
+          >
+            <Title items={thirdSectionTitleItems} controls={thirdSectionTitleControls} />
             <img
               className="mx-auto mt-16 hidden max-w-[620px] lg:block md:mt-12 md:max-w-[440px] sm:mt-8 sm:max-w-full"
               src={lgIllustration3}
