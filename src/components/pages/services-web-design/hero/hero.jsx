889be026ab7cfd8a1d61fd10b4199888ff91c9cf
Maksim Gorodov <silencer.web@gmail.com>
2022-03-14 15:13:53 +0400

    feat: add animation in the section Hero for the page Web Design

--- src/components/pages/services-web-design/hero/hero.jsx
+++ src/components/pages/services-web-design/hero/hero.jsx
@@ -1,46 +1,35 @@
-import React from 'react';
-// import React, { useEffect } from 'react';
-// import { useInView } from 'react-intersection-observer';
-// import { useRive, Layout, Fit, Alignment } from 'rive-react';
+import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useRive, Layout, Fit, Alignment } from 'rive-react';
 
-// import ImagePlaceholder from 'components/shared/image-placeholder';
+import ImagePlaceholder from 'components/shared/image-placeholder';
 
-import illustration from './images/illustration.svg';
+const Hero = () => {
+  const [wrapperRef, isWrapperInView] = useInView({ triggerOnce: true, threshold: 0.5 });
 
-const Hero = () => (
-  // const [wrapperRef, isWrapperInView] = useInView({ triggerOnce: true, threshold: 0.5 });
+  const { RiveComponent, rive } = useRive({
+    src: '/animations/pages/services-web-design/hero.riv',
+    autoplay: false,
+    layout: new Layout({
+      fit: Fit.FitWidth,
+      alignment: Alignment.Center,
+    }),
+  });
 
-  // const { RiveComponent, rive } = useRive({
-  //   src: '/animations/pages/services-web-design/hero.riv',
-  //   autoplay: false,
-  //   layout: new Layout({
-  //     fit: Fit.FitWidth,
-  //     alignment: Alignment.Center,
-  //   }),
-  // });
+  useEffect(() => {
+    if (isWrapperInView && rive) rive.play();
+  }, [isWrapperInView, rive]);
 
-  // useEffect(() => {
-  //   if (isWrapperInView && rive) rive.play();
-  // }, [isWrapperInView, rive]);
-
-  <section
-    className="safe-paddings bg-black pt-40 text-white lg:pt-32 md:pt-28 sm:pt-20"
-    // ref={wrapperRef}
-  >
-    <div className="container">
-      <h1 className="mx-auto max-w-[660px] text-center text-6xl font-normal leading-snug lg:max-w-[570px] lg:text-5xl md:max-w-[440px] md:text-4xl sm:max-w-[360px] sm:text-3xl xs:max-w-[290px] xs:text-2xl">
-        Get your idea shaped into <span className="text-red">Astonishing</span> website
-      </h1>
-      <div className="mt-10 flex justify-center lg:mt-8 lg:block md:mt-6 sm:mt-4">
-        <img
-          className="min-w-[1544px] 2xl:min-w-[calc(100vw-64px)] lg:min-w-0"
-          src={illustration}
-          alt=""
-          loading="eager"
-          aria-hidden
-        />
-      </div>
-      {/* <div className="mt-10 flex justify-center lg:mt-8 lg:block md:mt-6 sm:mt-4">
+  return (
+    <section
+      className="safe-paddings bg-black pt-40 text-white lg:pt-32 md:pt-28 sm:pt-20"
+      ref={wrapperRef}
+    >
+      <div className="container">
+        <h1 className="mx-auto max-w-[660px] text-center text-6xl font-normal leading-snug lg:max-w-[570px] lg:text-5xl md:max-w-[440px] md:text-4xl sm:max-w-[360px] sm:text-3xl xs:max-w-[290px] xs:text-2xl">
+          Get your idea shaped into <span className="text-red">Astonishing</span> website
+        </h1>
+        <div className="mt-10 flex justify-center lg:mt-8 lg:block md:mt-6 sm:mt-4">
           <ImagePlaceholder
             className="min-w-[1580px] 2xl:min-w-[calc(100vw-64px)] lg:min-w-0"
             width={1580}
@@ -49,9 +38,10 @@ const Hero = () => (
           >
             <RiveComponent />
           </ImagePlaceholder>
-        </div> */}
-    </div>
-  </section>
-);
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
