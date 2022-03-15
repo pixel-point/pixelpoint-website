07540860d186a91ecbb386106e0f530b113500c4
Maksim Gorodov <silencer.web@gmail.com>
2022-03-15 13:51:03 +0400

    fix: add some fixes

--- src/components/pages/services-web-design/hero/hero.jsx
+++ src/components/pages/services-web-design/hero/hero.jsx
@@ -5,7 +5,10 @@ import { useRive, Layout, Fit, Alignment } from 'rive-react';
 import ImagePlaceholder from 'components/shared/image-placeholder';
 
 const Hero = () => {
-  const [wrapperRef, isWrapperInView] = useInView({ triggerOnce: true, threshold: 0.5 });
+  const [animationWrapperRef, isAnimationWrapperInView] = useInView({
+    triggerOnce: true,
+    threshold: 0.8,
+  });
 
   const { RiveComponent, rive } = useRive({
     src: '/animations/pages/services-web-design/hero.riv',
@@ -17,19 +20,19 @@ const Hero = () => {
   });
 
   useEffect(() => {
-    if (isWrapperInView && rive) rive.play();
-  }, [isWrapperInView, rive]);
+    if (isAnimationWrapperInView && rive) rive.play();
+  }, [isAnimationWrapperInView, rive]);
 
   return (
-    <section
-      className="safe-paddings bg-black pt-40 text-white lg:pt-32 md:pt-28 sm:pt-20"
-      ref={wrapperRef}
-    >
+    <section className="safe-paddings bg-black pt-40 text-white lg:pt-32 md:pt-28 sm:pt-20">
       <div className="container">
         <h1 className="mx-auto max-w-[660px] text-center text-6xl font-normal leading-snug lg:max-w-[570px] lg:text-5xl md:max-w-[440px] md:text-4xl sm:max-w-[360px] sm:text-3xl xs:max-w-[290px] xs:text-2xl">
           Get your idea shaped into <span className="text-red">Astonishing</span> website
         </h1>
-        <div className="mt-10 flex justify-center lg:mt-8 lg:block md:mt-6 sm:mt-4">
+        <div
+          className="mt-10 flex justify-center lg:mt-8 lg:block md:mt-6 sm:mt-4"
+          ref={animationWrapperRef}
+        >
           <ImagePlaceholder
             className="min-w-[1580px] 2xl:min-w-[calc(100vw-64px)] lg:min-w-0"
             width={1580}
--- src/components/pages/services-web-development/hero/hero.jsx
+++ src/components/pages/services-web-development/hero/hero.jsx
@@ -7,7 +7,10 @@ import ImagePlaceholder from 'components/shared/image-placeholder';
 const STATE_MACHINE_NAME = 'State Machine';
 
 const Hero = () => {
-  const [wrapperRef, isWrapperInView] = useInView({ triggerOnce: true, threshold: 0.5 });
+  const [animationWrapperRef, isAnimationWrapperView] = useInView({
+    triggerOnce: true,
+    threshold: 0.8,
+  });
 
   const { RiveComponent, rive } = useRive({
     src: '/animations/pages/services-web-development/hero.riv',
@@ -20,26 +23,23 @@ const Hero = () => {
   });
 
   useEffect(() => {
-    if (isWrapperInView && rive) rive.play();
-  }, [isWrapperInView, rive]);
+    if (isAnimationWrapperView && rive) rive.play();
+  }, [isAnimationWrapperView, rive]);
 
   return (
-    <section
-      className="safe-paddings bg-black pt-40 text-white lg:pt-32 md:pt-28 sm:pt-20"
-      ref={wrapperRef}
-    >
+    <section className="safe-paddings bg-black pt-40 text-white lg:pt-32 md:pt-28 sm:pt-20">
       <div className="container">
         <h1 className="mx-auto max-w-[700px] text-center text-6xl font-normal leading-snug lg:max-w-[600px] lg:text-5xl md:max-w-[450px] md:text-4xl sm:max-w-[400px] sm:text-3xl xs:max-w-[300px] xs:text-2xl">
           Bring the power of <span className="text-red">JAMStack</span> to your project
         </h1>
-        <ImagePlaceholder
+        <div
           className="mx-auto mt-16 max-w-[1096px] lg:mt-12 md:mt-10 sm:mt-8"
-          width={1096}
-          height={600}
-          aria-hidden
+          ref={animationWrapperRef}
         >
-          <RiveComponent />
-        </ImagePlaceholder>
+          <ImagePlaceholder width={1096} height={600} aria-hidden>
+            <RiveComponent />
+          </ImagePlaceholder>
+        </div>
       </div>
     </section>
   );
