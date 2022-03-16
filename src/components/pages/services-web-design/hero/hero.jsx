e3174d4f617d257dd268f523eb94a7d2abacd355
Maksim Gorodov <silencer.web@gmail.com>
2022-03-16 13:51:00 +0400

    fix: add some fixes

--- src/components/pages/services-web-design/hero/hero.jsx
+++ src/components/pages/services-web-design/hero/hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
   return (
     <section className="safe-paddings bg-black pt-40 text-white lg:pt-32 md:pt-28 sm:pt-20">
       <div className="container">
-        <h1 className="mx-auto max-w-[660px] text-center text-6xl font-normal leading-snug lg:max-w-[570px] lg:text-5xl md:max-w-[440px] md:text-4xl sm:max-w-[360px] sm:text-3xl xs:max-w-[290px] xs:text-2xl">
+        <h1 className="relative z-10 mx-auto max-w-[660px] text-center text-6xl font-normal leading-snug lg:max-w-[570px] lg:text-5xl md:max-w-[440px] md:text-4xl sm:max-w-[360px] sm:text-3xl xs:max-w-[290px] xs:text-2xl">
           Get your idea shaped into <span className="text-red">Astonishing</span> website
         </h1>
         <div
