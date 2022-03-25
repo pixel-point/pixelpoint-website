80fdc670c64ca03929d2e2ea60b05db4bdda41b3
Maksim Gorodov <silencer.web@gmail.com>
2022-03-25 20:25:09 +0400

    fix: add some fixes

--- src/components/pages/case-studies/hero/hero.jsx
+++ src/components/pages/case-studies/hero/hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import ImagePlaceholder from 'components/shared/image-placeholder';
+
 import illustrationSm from './images/illustration-sm.svg';
 import illustration from './images/illustration.svg';
 
@@ -12,10 +14,18 @@ const Hero = () => (
       <p className="mt-2.5 text-2xl lg:text-xl md:text-lg sm:text-base">
         See how we have helped our customers achieve their goals
       </p>
-      <div className="mt-14 flex justify-center lg:mt-11 md:mt-10 sm:mt-4" aria-hidden>
-        <img className="sm:hidden" src={illustration} alt="" loading="eager" />
-        <img className="hidden sm:block" src={illustrationSm} alt="" loading="eager" />
-      </div>
+    </div>
+
+    <div className="mt-14 flex justify-center lg:mt-11 md:mt-10 sm:hidden" aria-hidden>
+      <ImagePlaceholder className="mx-2" width={1270} height={134}>
+        <img src={illustration} alt="" loading="eager" />
+      </ImagePlaceholder>
+    </div>
+
+    <div className="mt-4 hidden justify-center sm:flex" aria-hidden>
+      <ImagePlaceholder className="mx-1 w-full max-w-[414px]" width={466} height={90}>
+        <img src={illustrationSm} alt="" loading="eager" />
+      </ImagePlaceholder>
     </div>
   </section>
 );
