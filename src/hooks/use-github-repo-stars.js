1bfdb926b706953b9b3576ea3f6993ac89497fe8
Maksim Gorodov <silencer.web@gmail.com>
2022-03-25 19:50:09 +0400

    fix: fix

--- src/hooks/use-github-repo-stars.js
+++ src/hooks/use-github-repo-stars.js
@@ -1,17 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export default function useGithubRepoStars() {
-  const [githubStars] = useState(0);
+export default function useGithubRepoStars(username, repoName) {
+  const [githubStars, setGithubStars] = useState(0);
 
-  // useEffect(() => {
-  //   if (fetch) {
-  //     fetch(`https://api.github.com/repos/${username}/${repoName}`)
-  //       .then((response) => response.json())
-  //       .then(({ stargazers_count }) =>
-  //         setGithubStars(new Intl.NumberFormat('en-US').format(stargazers_count))
-  //       );
-  //   }
-  // }, [username, repoName]);
+  useEffect(() => {
+    if (fetch) {
+      fetch(`https://api.github.com/repos/${username}/${repoName}`)
+        .then((response) => response.json())
+        .then(({ stargazers_count }) =>
+          setGithubStars(new Intl.NumberFormat('en-US').format(stargazers_count))
+        );
+    }
+  }, [username, repoName]);
 
   return githubStars;
 }
