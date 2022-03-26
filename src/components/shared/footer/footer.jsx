857d6ba00c3c4b13e81dc7c64ba5826548f64356
Maksim Gorodov <silencer.web@gmail.com>
2022-03-26 17:15:05 +0400

    fix: add some fixes

--- src/components/shared/footer/footer.jsx
+++ src/components/shared/footer/footer.jsx
@@ -18,6 +18,19 @@ const links = [
   ],
 ];
 
+const linksSm = [
+  [
+    { text: 'Web design', to: '/services/web-design' },
+    { text: 'Web development', to: '/services/web-development' },
+    { text: 'Case studies', to: '/case-studies' },
+  ],
+  [
+    { text: 'Blog', to: '/blog' },
+    { text: 'Github', to: 'https://github.com/pixel-point' },
+    { text: 'Twitter', to: 'https://twitter.com/alex_barashkov' },
+  ],
+];
+
 const Footer = () => (
   <footer className="safe-paddings bg-black">
     <div className="container flex justify-between pt-10 pb-14 md:pt-0 sm:block sm:py-6">
@@ -35,9 +48,20 @@ const Footer = () => (
           info@pixelpoint.io
         </Link>
       </div>
-      <nav className="sm:grid-gap-x flex space-x-20 pt-2.5 sm:mt-10 sm:grid sm:grid-cols-2 sm:gap-y-6 sm:space-x-0 sm:pt-0">
+      <nav className="sm:grid-gap-x flex space-x-20 pt-2.5 sm:mt-10 sm:grid sm:grid-cols-2 sm:space-x-0 sm:pt-0">
         {links.map((links, index) => (
-          <ul className="space-y-8 sm:space-y-6" key={index}>
+          <ul className="space-y-8 sm:hidden" key={index}>
+            {links.map(({ to, text }, index) => (
+              <li className="text-[0px]" key={index}>
+                <Link to={to} size="base" theme="white">
+                  {text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        ))}
+        {linksSm.map((links, index) => (
+          <ul className="hidden space-y-6 sm:block" key={index}>
             {links.map(({ to, text }, index) => (
               <li className="text-[0px]" key={index}>
                 <Link to={to} size="base" theme="white">
