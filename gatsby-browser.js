54874106d8967ea4ffa0f07cfd6ec2fd9cf3ea5f
Alex Barashkov <lnikell@gmail.com>
2022-04-12 15:48:43 +0200

    chore: add plausible analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,10 @@
 
 // You can delete this file if you're not using it
 import './src/styles/main.css';
+
+// eslint-disable-next-line import/prefer-default-export
+export const onRouteUpdate = () => {
+  if (process.env.NODE_ENV === `production` && typeof window.plausible !== `undefined`) {
+    window.plausible(`pageview`);
+  }
+};
