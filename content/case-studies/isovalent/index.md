bdc8c024181925e222843666d4e4d383c7446a80
Maksim Gorodov <silencer.web@gmail.com>
2022-05-04 20:19:09 +0400

    feat: add Isovalent case study + overview field that will be used when there is no quote

---
+++ content/case-studies/isovalent/index.md
@@ -0,0 +1,36 @@
+---
+logo: logo.svg
+title: Isovalent
+description: Cloud native networking, security, and observability
+websiteUrl: https://isovalent.com
+githubUsername: cilium
+githubRepoName: cilium
+overview: Overview!
+services:
+  - Web Design
+  - Web Development
+  - Motion Design
+  - Illustrations
+stack:
+  - React
+  - Gatsby
+  - TailwindCSS
+  - Headless WordPress
+  - Lottie
+  - AddEvent
+  - HubSpot
+keynotes:
+  - Designed & developed eBPF, Cilium and Isovalent websites
+  - Filled up platforms with integrations necessary for business
+  - Established a clear editing workflow for marketing team
+cover: cover.jpg
+isOpenSource: true
+isFeatured: true
+position: 8
+---
+
+Isovalent is the enterprise [Cilium](https://pixelpoint.io/case-studies/cilium) edition, which enables cloud native networking, security, and observability.
+
+We've been working on their marketing website along with the Cilium and eBPF's ones, solving a typical enterprise problem - uniting different projects under a roof of a single brand, but keeping them distinctive and self-sufficient in terms of design in the same time.
+
+With the help of their content team, we completely reorganised Isovalent content and revamped the design. We made use of a cool toned palette that emphasizes the weight of the company and the value of their work. Speaking of that, we also lowered down the perception barrier by illustrating complex tech ideas with engaging animations.
--- src/components/pages/case-studies/case-studies/case-studies.jsx
+++ src/components/pages/case-studies/case-studies/case-studies.jsx
@@ -8,65 +8,73 @@ import { CASE_STUDIES_BASE_PATH } from 'constants/case-studies';
 const CaseStudies = ({ items }) => (
   <section className="safe-paddings mt-32 lg:mt-24 md:mt-16 sm:mt-11">
     <div className="container space-y-40 lg:space-y-36 md:space-y-24 sm:space-y-16">
-      {items.map(({ slug, frontmatter: { logo, title, description, quote, cover } }, index) => (
-        <article
-          className="grid-gap-x grid grid-cols-2 md:flex md:flex-col md:items-stretch"
-          key={index}
-        >
-          <h1 className="sr-only">{`${title} case study`}</h1>
-          <div className="lg:max-w-[436px] md:max-w-none">
-            <Link className="inline-block" to={`${CASE_STUDIES_BASE_PATH}${slug}`}>
-              <img
-                className="invert lg:translate-x-[-10%] lg:scale-[0.8]"
-                src={logo.publicURL}
-                alt={`${title} logo`}
+      {items.map(
+        ({ slug, frontmatter: { logo, title, description, overview, quote, cover } }, index) => (
+          <article
+            className="grid-gap-x grid grid-cols-2 md:flex md:flex-col md:items-stretch"
+            key={index}
+          >
+            <h1 className="sr-only">{`${title} case study`}</h1>
+            <div className="lg:max-w-[436px] md:max-w-none">
+              <Link className="inline-block" to={`${CASE_STUDIES_BASE_PATH}${slug}`}>
+                <img
+                  className="invert lg:translate-x-[-10%] lg:scale-[0.8]"
+                  src={logo.publicURL}
+                  alt={`${title} logo`}
+                  loading="lazy"
+                />
+              </Link>
+              <p className="mt-4 text-lg font-normal leading-snug lg:mt-3 lg:text-base sm:mt-2">
+                {description}
+              </p>
+              {quote ? (
+                <figure className="mt-5 border-t border-t-gray-4 pt-5 lg:mt-4 lg:pt-4">
+                  <blockquote>
+                    <p className="text-lg lg:text-base">{quote.text}</p>
+                  </blockquote>
+                  <figcaption className="mt-3 flex items-center">
+                    <GatsbyImage
+                      className="w-10 shrink-0 rounded-full"
+                      imgClassName="rounded-full"
+                      image={getImage(quote.authorPhoto)}
+                      alt={quote.authorName}
+                      loading="lazy"
+                    />
+                    <span className="ml-3.5 text-base font-normal">
+                      {quote.authorName} — {quote.authorPosition}
+                    </span>
+                  </figcaption>
+                </figure>
+              ) : (
+                <p className="mt-5 border-t border-t-gray-4 pt-5 text-lg lg:mt-4 lg:pt-4 lg:text-base">
+                  {overview}
+                </p>
+              )}
+              <Link
+                className="mt-8 lg:mt-5 md:mt-4"
+                to={`${CASE_STUDIES_BASE_PATH}${slug}`}
+                size="base"
+                theme="arrow-red"
+              >
+                {title} case study
+              </Link>
+            </div>
+            <Link
+              className="md:mt-11 sm:mt-8"
+              to={`${CASE_STUDIES_BASE_PATH}${slug}`}
+              aria-label={`${title} case study`}
+            >
+              <GatsbyImage
+                className="w-full rounded-2xl"
+                imgClassName="rounded-2xl"
+                image={getImage(cover)}
+                alt={`${title} website`}
                 loading="lazy"
               />
             </Link>
-            <p className="mt-4 text-lg font-normal leading-snug lg:mt-3 lg:text-base sm:mt-2">
-              {description}
-            </p>
-            <figure className="mt-5 border-t border-t-gray-4 pt-5 lg:mt-4 lg:pt-4">
-              <blockquote>
-                <p className="text-lg lg:text-base">{quote.text}</p>
-              </blockquote>
-              <figcaption className="mt-3 flex items-center">
-                <GatsbyImage
-                  className="w-10 shrink-0 rounded-full"
-                  imgClassName="rounded-full"
-                  image={getImage(quote.authorPhoto)}
-                  alt={quote.authorName}
-                  loading="lazy"
-                />
-                <span className="ml-3.5 text-base font-normal">
-                  {quote.authorName} — {quote.authorPosition}
-                </span>
-              </figcaption>
-            </figure>
-            <Link
-              className="mt-8 lg:mt-5 md:mt-4"
-              to={`${CASE_STUDIES_BASE_PATH}${slug}`}
-              size="base"
-              theme="arrow-red"
-            >
-              {title} case study
-            </Link>
-          </div>
-          <Link
-            className="md:mt-11 sm:mt-8"
-            to={`${CASE_STUDIES_BASE_PATH}${slug}`}
-            aria-label={`${title} case study`}
-          >
-            <GatsbyImage
-              className="w-full rounded-2xl"
-              imgClassName="rounded-2xl"
-              image={getImage(cover)}
-              alt={`${title} website`}
-              loading="lazy"
-            />
-          </Link>
-        </article>
-      ))}
+          </article>
+        )
+      )}
     </div>
   </section>
 );
@@ -81,6 +89,7 @@ CaseStudies.propTypes = {
         }).isRequired,
         title: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
+        overview: PropTypes.string,
         quote: PropTypes.exact({
           text: PropTypes.string.isRequired,
           authorName: PropTypes.string.isRequired,
@@ -90,7 +99,7 @@ CaseStudies.propTypes = {
               gatsbyImageData: PropTypes.object.isRequired,
             }).isRequired,
           }).isRequired,
-        }).isRequired,
+        }),
         cover: PropTypes.exact({
           childImageSharp: PropTypes.exact({
             gatsbyImageData: PropTypes.object.isRequired,
