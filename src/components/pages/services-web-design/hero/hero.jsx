b4bb67d8d6d53270bfe5f792ad7f8f192d12b317
Maksim Gorodov <silencer.web@gmail.com>
2022-06-02 12:57:35 +0500

    feat: update width of the animation

--- src/components/pages/services-web-design/hero/hero.jsx
+++ src/components/pages/services-web-design/hero/hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
         <h1 className="sr-only">Magnetize new users with astonishing design</h1>
         <div
           className={clsx(
-            'invisible absolute top-1/2 left-1/2 h-full w-full min-w-[1024px] max-w-[1920px] -translate-x-1/2 -translate-y-1/2 opacity-0 sm:min-w-[1024px]',
+            'invisible absolute top-1/2 left-1/2 h-full w-full min-w-[1024px] max-w-[1920px] -translate-x-1/2 -translate-y-1/2 opacity-0 sm:w-[325vw] sm:max-w-[1440px]',
             rive?.readyForPlaying && '!visible !opacity-100'
           )}
           ref={animationWrapperRef}
