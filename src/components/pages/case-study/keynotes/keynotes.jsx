585078f6cf638cfdaf11f4879733257010d663ab
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2026-04-21 20:54:03 +0800

    chore: add keynotes

--- src/components/pages/case-study/keynotes/keynotes.jsx
+++ src/components/pages/case-study/keynotes/keynotes.jsx
@@ -50,6 +50,7 @@ const icons = {
   ],
   novu: [WebsiteIcon, FingerprintIcon, EditingSquareBlueIcon],
   revenuecat: [WebsiteIcon, IntegrationsIcon, PerformanceIcon],
+  unkey: [WebsiteIcon, FingerprintIcon, EditingSquarePinkIcon],
   neon: [WebsiteIcon, EditingSquarePinkIcon, FingerprintIcon],
   parca: [WebsiteIcon, EditingSquarePinkIcon, PerformanceIcon],
   nayms: [WebsiteIcon, EditingSquarePinkIcon, PerformanceIcon],
