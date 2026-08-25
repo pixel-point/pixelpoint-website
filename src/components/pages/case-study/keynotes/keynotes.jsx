289d72e929a4b145f6ec76e82422e88276f5db2f
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2026-08-25 17:11:25 +0800

    chore: add phrasso case study

--- src/components/pages/case-study/keynotes/keynotes.jsx
+++ src/components/pages/case-study/keynotes/keynotes.jsx
@@ -76,6 +76,7 @@ const icons = {
   railway: [IntegrationsIcon, WebsiteIcon, SpeakerIcon],
   interhuman: [FingerprintIcon, WebsiteIcon, SpeakerIcon],
   bfl: [FingerprintIcon, WebsiteIcon, SpeakerIcon],
+  phrasso: [FingerprintSquareIcon, IntegrationsIcon, SpeakerIcon],
 };
 
 const Keynotes = ({ items, iconsName }) => (
